6c7e6c401b2f02ae7780653b30f636fd0f7d9610
unknown <sandeshlawhale@gmail.com>
2023-09-19 10:43:19 +0530

    first commit

---
+++ RandomColorGenerator/script.js
@@ -0,0 +1,12 @@
+const getColor = () => {
+
+    const randomNum = Math.floor(Math.random() * 16777215);
+    const randomCode = '#' + randomNum.toString(16);
+
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById('colorCode').innerText = randomCode;
+}
+
+document.getElementById('btn').addEventListener('click', getColor);
+
+getColor();
